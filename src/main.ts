22ea02feef8c0ce613de738c100ced7921cd75a8
tutu <t433567423@163.com>
2023-03-13 06:58:40 +0800

    feat: add something of login

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,22 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router'
-import 'jquery'
-import 'bootstrap/dist/js/bootstrap.min.js'
-import { Icon } from 'view-ui-plus'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import router from "./router";
+import "jquery";
+import "bootstrap/dist/js/bootstrap.min.js";
+import {
+  Icon,
+  Login,
+  UserName,
+  Password,
+  Submit,
+  Checkbox,
+} from "view-ui-plus";
 
-const app = createApp(App)
-const components = [Icon]
+const app = createApp(App);
+const components = [Icon, Login, UserName, Password, Submit, Checkbox];
 for (const cpn of components) {
-	app.component(cpn.name, cpn)
+  app.component(cpn.name, cpn);
 }
-app.use(router)
-app.mount('#app')
+app.use(router);
+app.mount("#app");
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -8,26 +8,45 @@
           margin-top: 50px;
           height: max-content;
           background-color: #e8eaec;
-        ">
+        "
+      >
+        <!-- 上方装饰 -->
         <div
-          class="d-flex flex-column align-items-center justify-content-center">
+          class="d-flex flex-column align-items-center justify-content-center"
+        >
           <div class="d-flex align-items-center">
             <Icon type="md-briefcase" size="55" color="#2d8cf0" />
             <h3 class="text-muted ms-2">涂涂の网盘</h3>
           </div>
+          <span class="text-muted small mx-3 mb">企业级网盘解决方案</span>
         </div>
+        <!-- 登录模块 -->
+        <Login @on-submit="handleSubmit">
+          <UserName v-model="formItem.username" name="username" />
+          <Password v-model="formItem.password" name="password" />
+          <div class="demo-auto-login">
+            <Checkbox v-model="formItem.isKeepLoading" size="large"
+              >自动登录</Checkbox
+            >
+            <a>忘记密码</a>
+          </div>
+          <Submit />
+        </Login>
       </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+const formItem = ref({ username: "", password: "", isKeepLoading: true });
+</script>
 
 <style lang="less" scoped>
 .login {
   height: 100vh;
   width: 100%;
-  background: url('../assets/img/login-bg.svg');
+  background: url("../assets/img/login-bg.svg");
   background-size: contain;
   // background-color: red;
 }
