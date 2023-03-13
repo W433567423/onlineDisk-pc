ffbf9e2d90316f9160fb977d4a6347f67a38b7ae
tutu <t433567423@163.com>
2023-03-13 13:56:44 +0800

    build: fixed eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,27 +10,12 @@ module.exports = {
   },
   parser: "vue-eslint-parser",
   parserOptions: {
-    ecmaVersion: 2022, // 使用的 ECMAScript  版本
+    ecmaVersion: "latest",
     sourceType: "module",
-    ecmaFeatures: {
-      jsx: true, // 开启jsx模板支持
-    },
   },
-  extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "plugin:prettier/recommended"],
+  extends: ["eslint:recommended", "plugin:prettier/recommended", "plugin:vue/vue3-essential", "plugin:@typescript-eslint/recommended"],
   rules: {
-    /* 常规规则 */
-    "spaced-comment": [2, "always"], // 注释后面必须写两个空格
-    "no-unused-vars": "off", // 忽略未使用的变量
-    "no-unreachable": "off", // 忽略return后未执行代码的提示
-    "no-empty": "off", // 忽略代码必须返回结果的限制
-    "no-prototype-builtins": "off", // 忽略不能直接使用Object原型的一些方法
-
-    /* vue规则 */
-    "vue/no-unused-components": "off", // vue忽略未使用组件提示
-    "vue/no-unused-vars": "off", // vue忽略未使用的变量
-    "vue/return-in-computed-property": "off", // vue忽略函数必须返回值
-    "vue/multi-word-component-names": "off", // vue忽略文件名建议的短横单词分割
-    "vue/no-reserved-component-names": "off", // vue忽略不能使用html标签当组件名的规则
-    "vue/comment-directive": "off",
+    "vue/multi-word-component-names": "off",
+    semi: [2],
   },
 };
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,33 +1,21 @@
 <template>
   <div class="login">
     <div class="d-flex justify-content-center" style="height: 100vh">
-      <div
-        class="rounded p-3"
-        style="
-          width: 380px;
-          margin-top: 50px;
-          height: max-content;
-          background-color: #e8eaec;
-        "
-      >
+      <div class="rounded p-3" style="width: 380px; margin-top: 50px; height: max-content; background-color: #e8eaec">
         <!-- 上方装饰 -->
-        <div
-          class="d-flex flex-column align-items-center justify-content-center"
-        >
+        <div class="d-flex flex-column align-items-center justify-content-center">
           <div class="d-flex align-items-center">
             <Icon type="md-briefcase" size="55" color="#2d8cf0" />
             <h3 class="text-muted ms-2">涂涂の网盘</h3>
           </div>
-          <span class="text-muted small mx-3 mb">企业级网盘解决方案</span>
+          <span class="text-muted small mx-3 mb-3">企业级网盘解决方案</span>
         </div>
         <!-- 登录模块 -->
         <Login @on-submit="handleSubmit">
           <UserName v-model="formItem.username" name="username" />
           <Password v-model="formItem.password" name="password" />
           <div class="demo-auto-login">
-            <Checkbox v-model="formItem.isKeepLoading" size="large"
-              >自动登录</Checkbox
-            >
+            <Checkbox v-model="formItem.isKeepLoading" size="large">自动登录</Checkbox>
             <a>忘记密码</a>
           </div>
           <Submit />
